a419523785374859ba742453cb800bb6549d9034
bielwolf <gabryellobo@gmail.com>
2025-08-17 11:24:07 -0300

    diretrizes

--- pac_man/pacman.h
+++ pac_man/pacman.h
@@ -1,3 +1,6 @@
+#ifndef _PACMAN_H_
+#define _PACMAN_H_
+
 #define CIMA 'w'
 #define BAIXO 's'
 #define ESQUERDA 'a'
@@ -13,5 +16,6 @@ int praondefantasmavai(int xatual, int yatual, int* xdestino, int* ydestino);
 void explodepilula();
 void explodepilula2(int x, int y, int somax, int somay, int qtd);
 
+#endif
 
 
